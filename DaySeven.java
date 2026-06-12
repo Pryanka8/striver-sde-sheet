52eada3666cd4abbee728715d9fb069a36df87b7
priyanka <ppant2222@gmail.com>
2026-06-13 01:29:15 +0530

    Recursion

---
+++ DaySeven.java
@@ -0,0 +1,27 @@
+// Q60 :The set [1, 2, 3, ..., n] contains a total of n! unique permutations. By listing and labeling all of the permutations in order, we get the following sequence for n = 3:     
+// find kth permutation sequence of n numbers.
+
+class DaySeven {
+    public String getPermutation(int n, int k) {
+        int fact = 1;
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 1; i < n; i++) {
+            fact = fact * i;
+            numbers.add(i);
+        }
+        numbers.add(n);
+        String ans = "";
+        k = k - 1;
+
+        while (true) {
+            ans = ans + numbers.get(k / fact);
+            numbers.remove(k / fact);
+            if (numbers.size() == 0) {
+                break;
+            }
+            k = k % fact;
+            fact = fact / numbers.size();
+        }
+        return ans;
+    }
+}
